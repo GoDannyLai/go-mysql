052989edc6734969a09761c69645e013dd71e6d5
nightKing <laijunshou@gmail.com>
2021-09-24 16:42:10 +0800

    update package path

--- query/query_test.go
+++ query/query_test.go
@@ -35,8 +35,8 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/percona/go-mysql/query"
-	_ "github.com/percona/go-mysql/test"
+	"github.com/GoDannyLai/go-mysql/query"
+	_ "github.com/GoDannyLai/go-mysql/test"
 )
 
 func TestFingerprintBasic(t *testing.T) {
